8303514a5522ead623425283cd4635a1e2892481
Daniel Pinedo <d.p@ieee.org>
2017-09-06 20:19:41 +0000

    working on assign1b.cpp

--- CS2/assign1b.cpp
+++ CS2/assign1b.cpp
@@ -1,6 +1,6 @@
 // Daniel Pinedo
 // Assignment 1b
-// due: September 6, 2017
+// due: September 11, 2017
 //
 
 #include <iostream>
@@ -21,68 +21,51 @@ struct Employee {
     short classif;
     int years;
     short educode;
-    
-  Employee(string fullname = "Not Set", char employeecode = 'X', 
-	int identification = 0, short classification = 0, int yrs = 51,
-	short educationalcode = 0) 
+   
+    Employee(string fullname = "Not Set", char employeecode = 'X', 
+		int identification = 0, short classification = 0, int yrs = 51,
+		short educationalcode = 0) 
 	{
 		name = fullname;
 		empcode = employeecode;
-		
+		ID = identification;
+		classif = classification;
+		years = yrs;
+		educode = educationalcode;
 	} 
 };
 
+// FUNCTION PROTOTYPES
+
+// open file path and read file contacts to array of struct Employee
+void inputFileToStruct(Employee data [], const string & InputFilename);
 
 int main()
 {
+	// declare array of struct Employee with a size = const MAX
 	Employee dataset [MAX];
+	
+	// Greeting & User Input
 	cout << fixed << showpoint << setprecision(2)
 		<< "Welcome!!! Please follow the prompts below \n";
-		
-	// Read input file path and open file
 	string InputFilename;
 	cout << "Enter full path to input file: ";
 	getline(cin, InputFilename);
 	cin.sync();
-	ifstream in(InputFilename); //open input file
-
-	// Verify that file was read successfully with conditional statement
-	// else send output "Failed to open file with name: " << InputFilename << ". Goodbye"
-	if (in.is_open()) {
-		size_t counter = 0;
-		while (in.peek() != EOF) {
-			string fullname = "Not Provided";
-			getline(in, fullname);
-			char employeecode = 'Z';
-			in >> employeecode;
-			int identification = 0;
-			in >> identification;
-			int classification = 0;
-			in >> classification;
-			int yrs = 51;
-			in >> yrs;
-			int educationalcode = 0;
-			in >> educationalcode;
-			dataset[counter] = Employee(fullname, employeecode, identification, 
-				classification, yrs, educationalcode);
-			counter++;
-		} // end while loop
-	} // end if statement
-	else {
-		cout << "Failed to open input file. " << endl;
-		exit(0);
-	}
 	
-	/* TODO
-	- if counter == 0, specify that input file is empty
-	*/
-	
-	in.close(); // close input file
+	// open file path and read file contacts to array of struct Employee
+	inputFileToStruct(dataset, InputFilename);
 
+
+    /* TODO
+    - below conditional statements will go into modules/functions
+    */
+    
 	string codename = "";
+	boolean badinput = false;
 	double basesalary = 0.00;
 	
-	switch (code)
+	switch (employeecode)
 	{
 	case 'S':
 		{
@@ -102,11 +85,16 @@ int main()
 		basesalary = 1500.00;
 		break;
 		}
+	default:
+		{
+		codename = "Invalid Code";
+		basesalary = 0.0;
+		badinput = true;
+		}
 	}
-
-	double classifadj = 0.00;
 	
-	switch (classif)
+	double classifadj = 0.00;
+	switch (classification)
 	{
 	case 1:
 		{
@@ -123,10 +111,16 @@ int main()
 		classifadj = (.20 * basesalary);
 		break;
 		}
+	default:
+		{
+		classifadj = 0.0;
+		badinput = true;
+		break;
+		}
 	}
 
 	double eduadj = 0.0;
-	switch (edu)
+	switch (educationalcode)
 	{
 	case 2:
 		{
@@ -143,23 +137,34 @@ int main()
 		eduadj = (.20 * basesalary);
 		break;
 		}
+	default:
+		{
+		eduadj = 0.0;
+		badinput = true;
+		break;
+		}
 	}
 
 	double seniorityadj = 0.0;
-	if (years >= 0 && years <= 10)
+	if (yrs >= 0 && yrs <= 10)
 	{
 		seniorityadj = (.05 * basesalary);
 	}
-	else if (years > 10 && years < 50)
+	else if (yrs > 10 && yrs < 50)
 	{
 		seniorityadj = (.01 * (years - 10) * basesalary);
 	}
 	else
 	{
-		cout << "Invalid entry for years of service" << "\n";
+		seniorityadj = 0.0;
+		badinput = true;
 	}
 
 	double totalsalary = basesalary + classifadj + eduadj + seniorityadj;
+	
+	/* TODO
+	- if badinput = true output that "This line contained bad input(s), unable to calculate salary"
+	*/
 	
 	cout << "Base Salary: " << basesalary << '\n';
 	cout << "Classification Adjustment: +" << classifadj << '\n';
@@ -176,3 +181,47 @@ int main()
 	return 0;
 }
 
+void inputFileToStruct(Employee data [], const string & InputFilename)
+{
+	ifstream in(InputFilename); //open input file
+
+	// Verify that file was read successfully with conditional statement
+	// else send output "Failed to open file with name: " << InputFilename << ". Goodbye"
+	if (in.is_open()) {
+		// fill array of struct employee
+		Employee dataset [MAX];
+		inputFileToStruct(dataset, InputFilename);
+		size_t counter = 0;
+		while ((in.peek() != EOF) && (counter < (MAX + 1))) {
+			string fullname = "Not Provided";
+			getline(in, fullname);
+			char employeecode = 'Z';
+			in >> employeecode;
+			int identification = 0;
+			in >> identification;
+			int classification = 0;
+			in >> classification;
+			int yrs = 51;
+			in >> yrs;
+			int educationalcode = 0;
+			in >> educationalcode;
+			if (counter < MAX) {
+				dataset[counter] = Employee(fullname, employeecode, identification, 
+					classification, yrs, educationalcode);
+				counter++;
+			}
+			else {
+				cout << "Input file exceeds 30 records. Successive records are truncated";
+			}
+		} // end while loop
+	} // end if statement
+	else {
+		cout << "Failed to open input file. " << '\n';
+		exit(0);
+	}
+	if (counter == 0) {
+		cout << "Your input file is empty, process aborted" << '\n';
+		exit(0);
+	}
+	in.close(); // close input file
+}
