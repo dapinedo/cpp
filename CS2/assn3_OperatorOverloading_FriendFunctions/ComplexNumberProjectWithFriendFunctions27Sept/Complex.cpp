6ab507dc306426a830f6d6b5de18f72dfa586b39
Daniel Pinedo <p1n3d0@outlook.com>
2017-10-01 20:31:17 -0700

    updated CS2/assn3* directory

---
+++ CS2/assn3_OperatorOverloading_FriendFunctions/ComplexNumberProjectWithFriendFunctions27Sept/Complex.cpp
@@ -0,0 +1,133 @@
+#include "Complex.h"
+
+
+
+Complex::Complex(double RL, double IM) :Real(RL), Imag(IM)
+{
+
+}
+
+const string Complex::toString() const
+{
+	ostringstream os;
+	os << fixed << showpoint << setprecision(4)
+		<< Real;
+
+	if (Imag >0.0)
+	{
+		os << " + " << Imag << "*I" << endl;
+	}
+	else if (Imag<0.0)
+	{
+		os << " - " << abs(Imag) << "*I" << endl;
+	}
+	else
+	{
+		os << endl;
+		// no code needed
+	}
+
+	return os.str();
+}
+
+const Complex operator + (const Complex & left, const Complex & right)
+{
+	double SReal = left.Real + right.Real;
+	double SImag = left.Imag + right.Imag;
+	return Complex(SReal, SImag);
+}
+
+const Complex operator - (const Complex & left, const Complex & right)
+{
+	double DReal = left.Real - right.Real;
+	double DImag = left.Imag - right.Imag;
+	return Complex(DReal, DImag);
+}
+
+const Complex operator * (const Complex & left, const Complex & right)
+{
+	throw "To be completed";
+}
+
+const Complex operator /(const Complex & left, const Complex & right)
+{
+	throw "To be completed";
+}
+
+ostream & operator<<(ostream & out, const Complex & C)
+{
+	out << C.toString();
+	return out;
+}
+
+istream & operator >>(istream & in, Complex & C)
+{
+	if (&in == &cin)
+	{
+		double RL, IM;
+		cout << "Enter The real part of complex number: ";
+		cin >> RL;
+		cout << "Enter the imaginary part of complex number: ";
+		cin >> IM;
+		C = Complex(RL, IM);
+	}
+	else
+	{
+		double RL, IM;
+		in >> RL >> IM;
+		C = Complex(RL, IM);
+	}
+
+	return in;
+}
+
+bool operator == (const Complex & left, const Complex & right)
+{
+	return (left.Real == right.Real) && (left.Imag == right.Imag);
+}
+
+bool operator != (const Complex & left, const Complex & right)
+{
+	return (left.Real != right.Real)|| (left.Imag != right.Imag);
+}
+
+double Complex::absolute() const
+{
+	return sqrt(Real*Real + Imag*Imag);
+}
+
+const Complex Complex::operator ++()
+{
+	return Complex(++Real, Imag);
+}
+
+const Complex Complex::operator ++(int NotUsed)
+{
+	// Save the values before post increment
+	double RL = Real;
+	double IM = Imag;
+	Real++;
+	return Complex(RL, IM);
+}
+
+const Complex Complex::operator --()
+{
+	throw " to be completed";
+}
+
+const Complex Complex::operator --(int NotUsed)
+{
+	throw " to be completed";
+}
+
+double Complex::getReal() const
+{
+	return Real;
+}
+double Complex::getImaginary() const
+{
+	return Imag;
+}
+Complex::~Complex()
+{
+}
