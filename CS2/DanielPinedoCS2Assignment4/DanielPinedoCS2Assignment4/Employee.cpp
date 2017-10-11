89a9663d3d2952a0b5f7c62260ae389169b05414
Daniel Pinedo <p1n3d0@outlook.com>
2017-10-11 13:24:17 -0700

    updated CS2/Assignment4/Employee.*

--- CS2/DanielPinedoCS2Assignment4/DanielPinedoCS2Assignment4/Employee.cpp
+++ CS2/DanielPinedoCS2Assignment4/DanielPinedoCS2Assignment4/Employee.cpp
@@ -2,18 +2,18 @@
 
 Employee::Employee(const string & fname, const string & lname) : FirstName(fname), LastName(lname)
 {
-	int fcounter = 0;
+	/*int fcounter = 0;
 	int lcounter = 0;
 	for (auto & c : FirstName) {
 		fcounter++;
 	}
 	for (auto & c : LastName) {
 		lcounter++;
-	}
-	if (fcounter < MIN_NUM_CHARS_NAMES) {
+	}*/
+	if (FirstName.length() < MIN_NUM_CHARS_NAMES) {
 		FirstName = "not set, value too short";
 	}
-	if (lcounter < MIN_NUM_CHARS_NAMES) {
+	if (LastName.length() < MIN_NUM_CHARS_NAMES) {
 		LastName = "not set, value too short";
 	}
 }
