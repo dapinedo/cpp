60a2d4f28e0cb610bddbba217fa7064d80c509e8
Daniel Pinedo <p1n3d0@outlook.com>
2017-10-30 16:21:27 -0700

    CS2/...Assignment6 completed tested OK

--- CS2/DanielPinedoCS2Assignment6/Driver3_1.cpp
+++ CS2/DanielPinedoCS2Assignment6/Driver3_1.cpp
@@ -18,7 +18,39 @@ int main()
 	return 0;
 }
 
+//---------------------
+//TEST OF MAIN FUNCTION
+//---------------------
+/*
+We will check each string to see if it is a palindrome now.
+How many strings do you want to enter now? 5
+Please enter a string to be checked: Race car
 
+Race car
+** IS a palindrome **
+
+Please enter a string to be checked: 'Twas the night before Christmas . . .
+
+'Twas the night before Christmas . . .
+** IS NOT a palindrome **
+
+Please enter a string to be checked: Egad!  A base life defiles a bad age.
+
+Egad!  A base life defiles a bad age.
+** IS a palindrome **
+
+Please enter a string to be checked: "Do nine men interpret?"  "Nine men," I nod.
+
+"Do nine men interpret?"  "Nine men," I nod.
+** IS a palindrome **
+
+Please enter a string to be checked: A man, a plan, a canal: Panama!
+
+A man, a plan, a canal: Panama!
+** IS a palindrome **
+
+Press any key to continue . . .
+*/
 //-------------------------------------------------------------
 void testString()
 {
@@ -49,46 +81,47 @@ void testString()
 
 void palindrome()
 {
-	ArrayQueue char_ArrayQueue;
-	cout << "We will enqueue some strings in the Queue now.\n";
+	cout << "We will check each string to see if it is a palindrome now.\n";
 	cout << "How many strings do you want to enter now? ";
 	size_t numstrings;
 	cin >> numstrings;
 	size_t counter = 0;
-	string val = string();
 	cin.ignore(128, '\n');
+	ArrayQueue char_ArrayQueue;
 	while (!char_ArrayQueue.isFull() && (counter < numstrings))
 	{
-		cout << "Please enter a string to be added to the Queue: ";
+		cout << "Please enter a string to be checked: ";
+		string val = "";
 		getline(cin, val);
 		ItemType Value;
 		Stack palindromeStack;
-		size_t numofchars = 0;
 		Value.string_data = val;
 		for (auto & c : val) {
 			if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
 				Value.Character = tolower(c);
-				cout << Value.Character << endl; // testing output to enqueue()
+				//cout << Value.Character << endl; // testing output to enqueue()
 				char_ArrayQueue.enqueue(Value);
 				palindromeStack.push(Value);
-				cout << palindromeStack.top().Character << endl; // testing output of push()
-				numofchars++;
+				//cout << palindromeStack.top().Character << endl; // testing output of push()
 			}
 		}
-		numofchars = 0;
-		cout << endl << Value.string_data;
+		cout << endl << "     " << Value.string_data;
 		bool ispalindrome = true;
-		while (!char_ArrayQueue.isEmpty() && ispalindrome) {
+		while (!char_ArrayQueue.isEmpty()) {
 			if (char_ArrayQueue.dequeue().Character != palindromeStack.top().Character) {
-				cout << endl << "** IS NOT a palindrome **" << endl;
+				palindromeStack.pop();
 				ispalindrome = false;
 			}
 			else {
 				palindromeStack.pop();
+				ispalindrome = true;
 			}
 		}
 		if (ispalindrome) {
-			cout << endl << "** IS a palindrome **" << endl;
+			cout << endl << "     ** IS a palindrome **" << endl << endl;
+		}
+		else {
+			cout << endl << "     ** IS NOT a palindrome **" << endl << endl;
 		}
 		counter++;
 	}
