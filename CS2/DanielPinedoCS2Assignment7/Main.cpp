46dcf66b17eb7b1495746ddcf5be22f4c20286b9
Daniel Pinedo <p1n3d0@outlook.com>
2017-11-06 16:17:48 -0800

    final update for CS2/...ASsignment7

--- CS2/DanielPinedoCS2Assignment7/Main.cpp
+++ CS2/DanielPinedoCS2Assignment7/Main.cpp
@@ -30,6 +30,59 @@ int main()
 	return 0;
 }
 
+//OUTPUT BELOW
+/*
+Creating a new Stack.
+We will push some integers on the stack now.
+Please enter an integer to be pushed on stack : 10
+**********>  Using the existing array to push. <*******************
+Number of items in stack now: 1
+The number of items on stack :1
+The capacity of stack array is: 1
+More data? Enter 0 to continue and 1 to exit: 0
+Please enter an integer to be pushed on stack : 9
+**********> Expanding the array to push. <*******************
+Number of items in stack now: 2
+The number of items on stack :2
+The capacity of stack array is: 2
+More data? Enter 0 to continue and 1 to exit: 0
+Please enter an integer to be pushed on stack : 8
+**********> Expanding the array to push. <*******************
+Number of items in stack now: 3
+The number of items on stack :3
+The capacity of stack array is: 3
+More data? Enter 0 to continue and 1 to exit: 0
+Please enter an integer to be pushed on stack : 7
+**********> Expanding the array to push. <*******************
+Number of items in stack now: 4
+The number of items on stack :4
+The capacity of stack array is: 4
+More data? Enter 0 to continue and 1 to exit: 0
+Please enter an integer to be pushed on stack : 6
+**********> Expanding the array to push. <*******************
+Number of items in stack now: 5
+The number of items on stack :5
+The capacity of stack array is: 5
+More data? Enter 0 to continue and 1 to exit: 0
+Please enter an integer to be pushed on stack : 5
+**********> Expanding the array to push. <*******************
+Number of items in stack now: 6
+The number of items on stack :6
+The capacity of stack array is: 6
+More data? Enter 0 to continue and 1 to exit: 1
+From Stack Copy Constructor.
+Making a copy of original stack.
+Topping and Popping the original Stack:
+From Stack Copy Constructor.
+Now printing the integers pushed on the stack.
+5 6 7 8 9 10
+Topping and Popping the copy stack.
+From Stack Copy Constructor.
+Now printing the integers pushed on the stack.
+5 6 7 8 9 10
+Press any key to continue . . .
+*/
+
 //----------------------------------------------------------------
 void PopAndPrintStack(Stack Stk)
 {
